876e5a3498b16fe0f9c2b73f6ad7d12c05d65040
Jared Watson <jjaredwatson@gmail.com>
2017-05-19 15:57:33 -0500

    added info to seed.js for schema

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -52,7 +52,7 @@ app.get('/api', function apiIndex(req, res) {
     baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
+      {method: "GET", path: "/api/campsites", description: "Data about campsites"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
     ]
   })
